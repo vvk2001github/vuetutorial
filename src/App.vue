c369a82b4685b1cd4c484ac19487bd129a83563b
kulyaginv <kulyaginv@gmail.com>
2023-09-01 19:30:11 +0500

    Add new Post to array posts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        <div :key="index" class="post" v-for="(post, index) in posts">
+    <div class="app">
+        <form>
+            <h4>Create post...</h4>
+            <input v-model="title" class="input" type="text" placeholder="Title">
+            <input v-model="body" class="input" type="text" placeholder="Description">
+            <button class="btn" @click.prevent="createPost">Create</button>
+        </form>
+        <div :key="post.id" class="post" v-for="post in posts">
             <div><strong>Title: </strong>{{ post.title }}</div>
             <div><strong>Description: </strong>{{ post.body }}</div>
         </div>
@@ -15,12 +21,23 @@ export default {
                 {id: 1, title: 'Title1', body: 'Post number 1'},
                 {id: 2, title: 'Title2', body: 'Post number 2'},
                 {id: 3, title: 'Title3', body: 'Post number 3'},
-            ]
+            ],
+            title: '',
+            body: '',
         }
     },
 
     methods: {
-        
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            };
+            this.posts.push(newPost);
+            this.title = '';
+            this.body = '';
+        }
     },
 }
 </script>
@@ -32,9 +49,34 @@ export default {
     box-sizing: border-box;
 }
 
+.app {
+    padding: 20px;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+}
+
 .post {
     padding: 15px;
     border: 2px solid teal;
+    margin-top: 15px;
+}
+
+.input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
     margin-top: 15px;
 }
 </style>
